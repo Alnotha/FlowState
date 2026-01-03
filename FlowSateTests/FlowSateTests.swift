e3b8d15bf407554921ce7ebb53161af827b11af3
alnotha <alnotha@gmail.com>
2026-01-02 23:24:36 -0600

    Initial Commit

---
+++ FlowSateTests/FlowSateTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowSateTests.swift
+//  FlowSateTests
+//
+//  Created by Alyan Tharani on 1/2/26.
+//
+
+import Testing
+@testable import FlowSate
+
+struct FlowSateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
